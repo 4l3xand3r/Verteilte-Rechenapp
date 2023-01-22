b5348bd6cedd283cb6d77c5e4cb85edca70ee10a
4l3xand3r <alex@alexander-heffner.de>
2023-01-23 00:01:57 +0100

    Revert "Added Tests"
    This reverts commit b64f4f4a6b13b586b78f6cb646c5785131c47bb7.

--- app/src/androidTest/java/de/heffner_alexander/rechenapp/ASAPTest.java
+++ app/src/androidTest/java/de/heffner_alexander/rechenapp/ASAPTest.java
@@ -2,7 +2,6 @@ package de.heffner_alexander.rechenapp;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
 import androidx.test.core.app.ActivityScenario;
 
@@ -10,12 +9,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-import java.util.Collections;
-
 import de.heffner_alexander.rechenapp.control.Client;
 import de.heffner_alexander.rechenapp.control.Controller;
 import de.heffner_alexander.rechenapp.control.Server;
-import kotlin.Pair;
 
 @RunWith(JUnit4.class)
 public class ASAPTest {
@@ -64,33 +60,11 @@ public class ASAPTest {
                 1.0
         );
 
+        client.sendDataToServer(Controller.getResults());
+
         System.out.println(Controller.getResults().size());
 
-        assertEquals(1, Controller.getResults().size(), 0.0);
-
-        activity.close();
-        loadingActivity.close();
-        resultsActivity.close();
-    }
-
-    @Test
-    public void serverSendTrueTest() {
-        ActivityScenario<MainActivity> activity = ActivityScenario.launch(MainActivity.class);
-        ActivityScenario<LoadingActivity> loadingActivity = ActivityScenario.launch(LoadingActivity.class);
-        ActivityScenario<ResultsActivity> resultsActivity = ActivityScenario.launch(ResultsActivity.class);
-
-        Server server = new Server();
-        Client client = new Client();
-
-        server.initializeASAP();
-        client.initializeASAP();
-
-        assertTrue(server.sendDataToClients(
-                "1+x",
-                1.0,
-                1.0,
-                1.0
-        ));
+        assertEquals(2, Controller.getResults().size(), 0.0);
 
         activity.close();
         loadingActivity.close();
--- app/src/main/java/de/heffner_alexander/rechenapp/control/Client.java
+++ app/src/main/java/de/heffner_alexander/rechenapp/control/Client.java
@@ -64,9 +64,7 @@ public class Client implements IClientConn, ASAPEnvironmentChangesListener, ASAP
                     ASAPSerialization.writeCharSequenceParameter(device, bos);
                     ASAPSerialization.writeCharSequenceParameter(builder.toString(), bos);
                     peer.sendOnlineASAPMessage(appFormat, uri, bos.toByteArray());
-                    Controller.showResults();
-                    Controller.returnToStart();
-                    return true;
+                    successful = true;
                 } catch (IOException | ASAPException e) {
                     e.printStackTrace();
                     return false;
@@ -106,7 +104,6 @@ public class Client implements IClientConn, ASAPEnvironmentChangesListener, ASAP
 
                     if (message.startsWith(this.peer.getPeerID().toString())) {
                         String instructions = message.replaceFirst(this.peer.getPeerID().toString(), "");
-                        Controller.jumpToLoading();
                         Controller.startCalculations(instructions);
                     }
                 } catch (ASAPException e) {
